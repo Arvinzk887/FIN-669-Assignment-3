864e614c0bd0919ac4dd05a8225edcd42417af6b
Arvinzk887 <arvindhazk887@gmail.com>
2025-09-25 17:30:55 -0700

    Added the plot for BEP

--- code3.py
+++ code3.py
@@ -13,3 +13,16 @@ BEPS = Fixed/(CM/SellPrice)
 
 print(f'\n\n Your break even point in units is: {BEP:2f}'
 + f'\n\n Your break even point in sales is: {BEPS:2f}')
+
+plt.plot([0, 2 * BEP],[Fixed, Fixed + Variable * 2 * BEP],
+color='red',label='Costs')
+plt.plot([0, 2 * BEP],[0, SellPrice * 2 * BEP],
+color='green',label='Revenue')
+plt.legend()
+plt.title('Break-Even Analysis')
+plt.ylabel('Sales in Dollars')
+plt.xlabel('Units Sold')
+plt.ylim(bottom=0)
+plt.xlim(left=0)
+plt.xlim(right=2*BEP)
+plt.show()
